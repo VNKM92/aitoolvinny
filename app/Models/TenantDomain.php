a115bd030f3efe5819c35ca3280d68aa4ddd68e3
VNKM92 <vinaymaurya134@gmail.com>
2026-07-24 13:29:34 +0530

    Initiate Website

---
+++ app/Models/TenantDomain.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class TenantDomain extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'tenant_id',
+        'domain',
+        'is_primary',
+    ];
+
+    protected $casts = [
+        'is_primary' => 'boolean',
+    ];
+
+    public function tenant(): BelongsTo
+    {
+        return $this->belongsTo(Tenant::class);
+    }
+}
